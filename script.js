95a39d6ce0d4cb470b2b392ceb13e7a0cd1dfcfd
Shiv-96 <kshiv.dot@gmail.com>
2022-11-25 14:39:25 +0530

    Navbar section and Home section Completed

---
+++ script.js
@@ -0,0 +1,27 @@
+const list = document.querySelector('.header .nav-bar .nav-list .list');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a')
+const header = document.querySelector('.header');
+list.addEventListener('click', () =>{
+    list.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+})
+
+document.addEventListener('scroll', () =>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundColor = '#29323c';
+    }
+    else{
+        header.style.backgroundColor = 'transparent';
+    }
+})
+
+menu_item.forEach(item => {
+
+    item.addEventListener('click', () => {
+        list.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    })
+
+})
