f95b5bd8fadf4dc28835ad6629081517ebad3b1d
Shiv-96 <kshiv.dot@gmail.com>
2023-05-17 16:20:26 +0530

    Some projects are added and resume is updated

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-const list = document.querySelector('.header .nav-bar .nav-list .list');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a')
-const header = document.querySelector('.header');
-list.addEventListener('click', () =>{
-    list.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
-})
+const list = document.querySelector(".header .nav-bar .nav-list .list");
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header");
+list.addEventListener("click", () => {
+  list.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
 
-document.addEventListener('scroll', () =>{
-    var scroll_position = window.scrollY;
-    if(scroll_position > 250){
-        header.style.backgroundColor = '#29323c';
-    }
-    else{
-        header.style.backgroundColor = 'transparent';
-    }
-})
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
 
-menu_item.forEach(item => {
-
-    item.addEventListener('click', () => {
-        list.classList.toggle('active');
-        mobile_menu.classList.toggle('active');
-    })
-
-})
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    list.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
 
 // Typing animation
-var typed = new Typed(".typing2",{
-    strings: ["Full Stack Java Developer", "Backend Developer","Java Developer"],
-    typeSpeed:100,
-    backSpeed:60,
-    loop: true
+var typed = new Typed(".typing2", {
+  strings: ["Full Stack Java Developer", "Backend Developer", "Java Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
 });
-var typed = new Typed(".typing",{
-    strings: ["Full Stack Java Developer", "Backend Developer","Java Developer"],
-    typeSpeed:100,
-    backSpeed:60,
-    loop: true
+var typed = new Typed(".typing", {
+  strings: ["Full Stack Java Developer", "Backend Developer", "Java Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
 });
 
-function openNewWindow(){
-    window.open('https://drive.google.com/file/d/1MYNd24ohnsN8qGSlVuPpJP1EYnzP6D1W/view');
+function openNewWindow() {
+  window.open(
+    "https://drive.google.com/file/d/1SlJB4C0Sg-fUdDz-I694e5rmeI0plGjk/view?usp=sharing"
+  );
 }
